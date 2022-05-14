1f16fb75a51dff741311a7a797222b553289bd9a
João Pedro A <joaofernandes.dev@hotmail.com>
2022-05-13 21:07:17 -0300

    Header

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,5 +1,12 @@
+/* eslint-disable react/prop-types */
 // import React from 'react'
+import images from '../../constants/images.js'
 
-const SubHeading = () => <div>SubHeading</div>
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={images.spoon} alt="spoon" />
+  </div>
+)
 
 export default SubHeading
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,31 @@
+/* eslint-disable import/extensions */
 // import React from 'react'
 
-import './Header.css'
+import './Header.scss'
+import images from '../../constants/images.js'
+import { SubHeading } from '../../components'
 
-const Header = () => <div>Header</div>
+const Header = () => {
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavour" />
+        <h1>The Key to Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: '2rem 0' }}>
+          Cras malesuada aliquam purus in laoreet. Sed suscipit, ligula vitae
+          commodo euismod, est ante laoreet neque, in placerat dui dui at elit.
+          Donec nisl eros, ultrices vitae nulla ac, luctus porttitor metus.
+        </p>
+        <button type="button" className="custom__button">
+          Explore Menu
+        </button>
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header img" />
+      </div>
+    </div>
+  )
+}
 
 export default Header
